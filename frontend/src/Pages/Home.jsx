e910c3adb2ac7bfad3c91effb7324645c82cc96f
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2023-11-13 22:31:39 +0100

    setting loading while feching api

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -6,40 +6,44 @@ import { useEffect, useState } from "react";
 
 const Home = () => {
   const [workouts, setWorkouts] = useState(null);
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
     const fecthWorkOut = async () => {
-
-
-        try {
-            const response = await fetch("http://localhost:4000/workout/getAllWork");
-      const json = await response.json();
-      console.log(json);
-      if (response?.ok) {
-        setWorkouts(json);
-      }
-            
-        } catch (error) {
-            console.log(error);
-            
+      try {
+        setLoading(true);
+        const response = await fetch(
+          "http://localhost:4000/workout/getAllWork"
+        );
+        const json = await response.json();
+        console.log(json);
+        if (response?.ok) {
+          setWorkouts(json);
         }
-      
-     
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
     };
 
-    fecthWorkOut()
+    fecthWorkOut();
   }, []);
   return (
     <div>
       <Navbar></Navbar>
       <p>Home</p>
       <div className="workout">
+        {loading && <span>Loaing...</span>}
         {workouts &&
           workouts.map((workout) => {
             return (
-                <>
-                <p key={workout._id}> {workout.title}</p>;
-                </>
-            ) 
+              <>
+                <p key={workout._id}> {workout.title}</p>
+                <p key={workout._id}> {workout.reps}</p>
+                <p key={workout._id}> {workout.load}</p>
+              </>
+            );
           })}
       </div>
     </div>
